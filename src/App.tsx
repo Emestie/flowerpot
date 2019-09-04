65b63f70b760b4ce45483bf8a8edc089aeefafe4
Emestie <emestie@gmail.com>
2019-09-04 21:59:42 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+//const { dialog } = require("electron");
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to 1reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+                <button
+                    onClick={() => {
+                      //  dialog.showOpenDialog({ properties: ["openFile", "openDirectory", "multiSelections"] });
+                    }}
+                >
+                    alert
+                </button>
+                <button
+                    onClick={() => {
+                        let notif = new Notification("lalala");
+                    }}
+                >
+                    notif
+                </button>
+            </header>
+        </div>
+    );
+};
 
 export default App;
