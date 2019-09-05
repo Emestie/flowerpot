6b91f34e7e98cc9de4b3f9653ece1ae91f9febb6
Emestie <emestie@gmail.com>
2019-09-05 13:37:03 +0300

    example added

---
+++ src/models/Pair.ts
@@ -0,0 +1,13 @@
+export class Pair {
+    public queueId: string;
+    public projectId: string;
+    public queueName: string;
+    public projectName: string;
+
+    constructor(pair: any) {
+        this.queueId = ";;";
+        this.projectId = ";;";
+        this.queueName = ";;";
+        this.projectName = ";;";
+    }
+}
