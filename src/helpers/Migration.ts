1a9aac130bd5defee20de1c61e756f569ddbabd3
Emestie <emestie@gmail.com>
2020-08-25 09:15:07 +0300

    array mask

--- src/helpers/Migration.ts
+++ src/helpers/Migration.ts
@@ -9,9 +9,9 @@ export default class Migration {
         //check if tfs path contains collection
         const path = store.settings.tfsPath;
         if (path.indexOf("Collection") === -1) return;
-        
-        console.log('Migration v0_2_12_to_v0_2_13');
-        
+
+        console.log("Migration v0_2_12_to_v0_2_13");
+
         const pieces = path.split("/");
 
         const collectionName = pieces[pieces.length - 2];
@@ -19,13 +19,13 @@ export default class Migration {
         store.settings.tfsPath = pieces.filter((x) => x.indexOf("Collection") === -1).join("/");
 
         //set former collection to all queries if there is no any
-        store.settings.queries = store.settings.queries.map((x) => ({ ...x, collectionName: collectionName }));
+        store.settings.queries = (store.settings.queries || []).map((x) => ({ ...x, collectionName: collectionName }));
 
-        store.settings.lists.permawatch = store.settings.lists.permawatch.map((x) => ({ ...x, collection: x.collection || collectionName }));
-        store.settings.lists.deferred = store.settings.lists.deferred.map((x) => ({ ...x, collection: x.collection || collectionName }));
-        store.settings.lists.favorites = store.settings.lists.favorites.map((x) => ({ ...x, collection: x.collection || collectionName }));
-        store.settings.lists.hidden = store.settings.lists.hidden.map((x) => ({ ...x, collection: x.collection || collectionName }));
-        store.settings.lists.pinned = store.settings.lists.pinned.map((x) => ({ ...x, collection: x.collection || collectionName }));
+        store.settings.lists.permawatch = (store.settings.lists.permawatch || []).map((x) => ({ ...x, collection: x.collection || collectionName }));
+        store.settings.lists.deferred = (store.settings.lists.deferred || []).map((x) => ({ ...x, collection: x.collection || collectionName }));
+        store.settings.lists.favorites = (store.settings.lists.favorites || []).map((x) => ({ ...x, collection: x.collection || collectionName }));
+        store.settings.lists.hidden = (store.settings.lists.hidden || []).map((x) => ({ ...x, collection: x.collection || collectionName }));
+        store.settings.lists.pinned = (store.settings.lists.pinned || []).map((x) => ({ ...x, collection: x.collection || collectionName }));
 
         store.updateSettings();
     }
