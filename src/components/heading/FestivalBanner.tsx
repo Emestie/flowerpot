457bcb60f25f72dc5fb6cd0bef458b36db7d92b1
Valery Murashko <emestie@gmail.com>
2024-05-08 17:04:38 +0300

    если нет фестиваля, показывать логотип приложения

--- src/components/heading/FestivalBanner.tsx
+++ src/components/heading/FestivalBanner.tsx
@@ -1,14 +1,23 @@
 import { useSelector } from "react-redux";
+import flowerpotLogo from "../../assets/flower4.png";
+import { IFestivalDescriptor } from "/@/helpers/Festival";
 import { appSelector } from "/@/redux/selectors/appSelectors";
+
+const defaultLogo: IFestivalDescriptor = {
+    icon: { width: 28, height: 28, left: 16, top: 16, offset: 0, path: flowerpotLogo },
+    dateFrom: "",
+    dateTo: "",
+    name: "",
+};
 
 export default () => {
     const { currentFestival } = useSelector(appSelector);
 
-    if (!currentFestival) return null;
+    const _currentFestival = currentFestival ?? defaultLogo;
 
     const {
         icon: { height, left, path, top, width },
-    } = currentFestival;
+    } = _currentFestival;
 
     return (
         <div style={{ position: "absolute", top: top, left: left, width: width, height: height }}>
