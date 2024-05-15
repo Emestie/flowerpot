ef7259fb7d70a01d0234433b8462791841e7e2e6
Valery Murashko <emestie@gmail.com>
2024-05-15 15:44:02 +0300

    changelog

--- src/hooks/useHighlights.ts
+++ src/hooks/useHighlights.ts
@@ -3,5 +3,5 @@ import { useQuickSearchStore } from "../zustand/quick-search";
 export function useHighlights() {
     const value = useQuickSearchStore((s) => s.value);
 
-    return (value ?? "").split(";").filter((x) => !!x);
+    return (value ?? "").split(";").filter((x) => !!x.trim());
 }
