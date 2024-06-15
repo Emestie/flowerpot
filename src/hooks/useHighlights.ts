13577178c632bea06d48f020ec2fb5441f3e64c6
Valery Murashko <emestie@gmail.com>
2024-06-15 22:34:57 +0300

    quick search capital letter fix

--- src/hooks/useHighlights.ts
+++ src/hooks/useHighlights.ts
@@ -3,5 +3,8 @@ import { useQuickSearchStore } from "../zustand/quick-search";
 export function useHighlights() {
     const value = useQuickSearchStore((s) => s.value);
 
-    return (value ?? "").split(";").filter((x) => !!x.trim());
+    return (value ?? "")
+        .toLowerCase()
+        .split(";")
+        .filter((x) => !!x.trim());
 }
